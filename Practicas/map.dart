f417004f5e7df36af42c20045d6c38707d2f794c
Edryx117 <83787617+Edryx117@users.noreply.github.com>
2023-02-03 12:47:23 -0600

    Add files via upload

---
+++ Practicas/map.dart
@@ -0,0 +1,12 @@
+void main(){
+ Map persona = {
+   'nombre': 'Edy',
+   'edad' : 29,
+   'soltero': false,
+   true: false,
+   1: 100,
+   2: 500
+ };
+  print(persona [1]);
+  
+}
---
+++ Practicas/mapDynamic.dart
@@ -0,0 +1,10 @@
+void main(){
+ Map <String, dynamic> persona = {
+   'nombre': 'Edy',
+   'edad' : 29,
+   'soltero': false,
+ };
+ persona.addAll({'segundoNombre': 'Juan'});
+  print (persona);
+  
+}
